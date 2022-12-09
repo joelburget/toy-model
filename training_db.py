b7272f14e906a09e6f6ecad77b564902759e6656
Joel Burget <joelburget@gmail.com>
2022-12-09 13:02:38 -0500

    Add training_db.py.

---
+++ training_db.py
@@ -0,0 +1,48 @@
+import sqlite3
+from data import TrainConfig, TrainResult
+
+con = sqlite3.connect("training.db")
+cur = con.cursor()
+
+
+def initialize_db():
+    cur.execute(
+        "CREATE TABLE training_run(run_no, name, sparsity, importance, points, steps, task)"
+    )
+
+
+def get_next_num() -> int:
+    (count,) = cur.execute("SELECT COUNT(*) FROM training_run").fetchone()
+    return count + 1
+
+
+def insert_conf(conf: TrainConfig):
+    cur.execute(
+        "INSERT INTO training_run VALUES(?, ?, ?, ?, ?, ?, ?)",
+        (
+            get_next_num(),
+            conf.model_name,
+            conf.s,
+            conf.i,
+            conf.points,
+            conf.steps,
+            conf.task,
+        ),
+    )
+    con.commit()
+
+
+def insert_train_result(train_result):
+    n = get_next_num()
+    train_result.save(f"train_results/{n}")
+    insert_conf(train_result.config)
+
+
+def get_config(n: int) -> TrainConfig:
+    res = cur.execute("SELECT * FROM training_run WHERE run_no = ?", (n,))
+    _, name, s, i, points, steps, task = res.fetchone()
+    return TrainConfig(name, s, i, points, steps, task)
+
+
+def get_result(n: int) -> TrainResult:
+    return TrainResult.load(f"train_results/{n}")
