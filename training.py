1f7846a365cadb70ffc704f810afe9fb75d7c7eb
Joel Burget <joelburget@gmail.com>
2022-12-11 16:29:58 -0800

    Add cube training script.

---
+++ training.py
@@ -0,0 +1,56 @@
+from toy_model import train_model
+from data import TrainConfig, sparsities
+from training_db import insert_train_result
+from multiprocessing import Pool, Lock
+import timeit
+from unittest import mock
+
+act_fns = ["ReLU", "GeLU", "SoLU"]
+DUPS = 5
+
+
+def notqdm(iterable, *args, **kwargs):
+    """
+    replacement for tqdm that just passes back the iterable
+    useful to silence `tqdm` in tests
+    """
+    return iterable
+
+
+@mock.patch("tqdm.auto.tqdm", notqdm)
+def go(act_fn, use_ln, s):
+    start = timeit.default_timer()
+    print(f"starting {act_fn}, {use_ln}, {s}")
+    config = TrainConfig(
+        "ToyModel" if not use_ln else "LayerNormToyModel",
+        s=s,
+        i=0.8,
+        task="ID",
+        steps=50_000,
+        act_fn=act_fn,
+    )
+    train_result = train_model(config)
+    stop = timeit.default_timer()
+    print(f"{act_fn}, {use_ln}, {s} complete (after {stop - start}s), inserting")
+    insert_train_result(train_result, lock)
+
+
+def init_pool_processes(the_lock):
+    """Initialize each process with a global variable lock."""
+    global lock
+    lock = the_lock
+
+
+if __name__ == "__main__":
+    lock = Lock()
+    configurations = [
+        (act_fn, use_ln, s)
+        for act_fn in act_fns
+        for use_ln in (False, True)
+        for s in sparsities
+    ] * DUPS
+
+    with Pool(processes=16, initializer=init_pool_processes, initargs=(lock,)) as pool:
+        pool.starmap(go, configurations)
+        pool.close()
+        pool.join()
--- training_db.py
+++ training_db.py
@@ -65,14 +65,19 @@ def insert_conf(conf: TrainConfig):
     con.commit()
 
 
-def insert_train_result(train_result):
-    n = get_next_num()
-    train_result.save(f"train_results/{n}")
-    insert_conf(train_result.config)
+def insert_train_result(train_result, lock=None):
+    try:
+        if lock:
+            lock.acquire()
+        n = get_next_num()
+        train_result.save(f"train_results/{n}")
+        insert_conf(train_result.config)
+    finally:
+        if lock:
+            lock.release()
 
 
-def get_config(n: int) -> TrainConfig:
-    res = cur.execute("SELECT * FROM training_run WHERE run_no = ?", (n,))
+def populate_train_config(row):
     (
         _,
         name,
@@ -85,11 +90,20 @@ def get_config(n: int) -> TrainConfig:
         act_fn,
         args,
         _,
-    ) = res.fetchone()
+    ) = row
     return TrainConfig(
         name, s, i, points, steps, task, regularization_coeff, act_fn, json.loads(args)
     )
 
 
+def get_config(n: int) -> TrainConfig:
+    res = cur.execute("SELECT * FROM training_run WHERE run_no = ?", (n,))
+    populate_train_config(res.fetchone())
+
+
 def get_result(n: int) -> TrainResult:
     return TrainResult.load(f"train_results/{n}")
+
+
+if __name__ == "__main__":
+    initialize_db()
