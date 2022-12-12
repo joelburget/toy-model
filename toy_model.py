b4ca72744469ff7251b2e0c75ab2ce6de74be3b1
Joel Burget <joelburget@gmail.com>
2022-12-11 17:12:20 -0800

    Add some WIP-ish toy_model stuff.

--- toy_model.py
+++ toy_model.py
@@ -168,6 +168,40 @@ def sankey(title: str, sources, targets, values, node_names, layer_shapes):
     )
 
 
+def trace_sankey(layers: List[torch.tensor]):
+    nodes_seen = 0
+    sources, targets, values, node_names = [], [], [], []
+
+    # node_names += [f"I{i}={x:.3f}" for i, x in enumerate(inputs)]
+    # prev_layer_acts = inputs
+
+    for layer_num, layer in enumerate(layers):
+        n_rows, n_cols = layer.shape
+        # activations = layer @ prev_layer_acts
+        layer_name = chr(layer_num + ord("A"))
+        node_names += [f"{layer_name}{i}={x:.3f}" for i, x in enumerate(activations)]
+        for i in range(n_cols):
+            for j in range(n_rows):
+                sources.append(nodes_seen + i)
+                # targets.append(nodes_seen + len(prev_layer_acts) + j)
+                # values.append(layer[j, i] * prev_layer_acts[i])
+        # nodes_seen += len(prev_layer_acts)
+        # prev_layer_acts = activations
+
+    # XXX transpose?
+    layer_shapes = [layers[0].shape[1], layers[0].shape[0]]
+    layer_shapes += [layer.shape[0] for layer in layers[1:]]
+
+    return sankey(
+        "Activations for Given Inputs",
+        sources,
+        targets,
+        values,
+        node_names,
+        layer_shapes,
+    )
+
+
 def weights_sankey(layers: List[NDArray]):
     # layers: e.g. [m, n], [n, o]
     # e.g. [neurons, features], [features, neurons]
@@ -298,14 +332,24 @@ class HiddenLayerModelVariation(nn.Module):
         self.W2 = nn.Parameter(torch.randn(features, neurons))
         self.b = nn.Parameter(torch.randn(features))
         self.act_fn = act_fn
+        self.layer_cache = []
+
+    def cache(self, layer):
+        self.layer_cache.append(layer)
+        return layer
 
     def forward(self, x):
         f = act_fn(self.act_fn)
-        x = einops.einsum(self.W1, x, "inner outer, batch outer -> batch inner")
-        x = f(x)
-        x = einops.einsum(self.W2, x, "outer inner, batch inner -> batch outer")
-        x = x + self.b
-        x = f(x)
+        self.cache(x)
+        x = self.cache(
+            einops.einsum(self.W1, x, "inner outer, batch outer -> batch inner")
+        )
+        x = self.cache(f(x))
+        x = self.cache(
+            einops.einsum(self.W2, x, "outer inner, batch inner -> batch outer")
+        )
+        x = self.cache(x + self.b)
+        x = self.cache(f(x))
         return x, 0
 
     @staticmethod
@@ -364,6 +408,7 @@ class MlpModel(nn.Module):
         )
         return x, 0
 
+    # TODO: plot MLP
     @staticmethod
     def plot(train_result):
         model = train_result.model
@@ -395,3 +440,14 @@ class ResidualModel(nn.Module):
             self.W_U, x, "d_model features, batch d_model -> batch features"
         )
         return x, 0
+
+    # TODO: plot MLP
+    @staticmethod
+    def plot(train_result):
+        model = train_result.model
+        w_e = model.W_E.detach().numpy()
+        w_u = model.W_U.detach().numpy()
+        px.imshow(w_e, title="W_E").show()
+        stack_plot(w_e.T).show()
+        px.imshow(w_u.T, title="W_U").show()
+        stack_plot(w_u).show()
