f1f9695b5210838ef7c54e24c4fc6f7b20a13e26
Joel Burget <joelburget@gmail.com>
2022-12-16 19:36:24 -0800

    Dashboard updates: checkpoints, run IDs, etc.

--- toy_model_dash.py
+++ toy_model_dash.py
@@ -1,7 +1,9 @@
 import json
+import os
 import sqlite3
 
 import dash_daq as daq
+import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 import torch
@@ -38,7 +40,7 @@ def foldable_section(title, *children):
 
 
 def slider():
-    return daq.Slider(min=0, max=10, value=0, marks={"0": "0"})
+    return daq.Slider(min=-10, max=10, value=0, marks={"0": "0"})
 
 
 app.layout = rows(
@@ -54,6 +56,15 @@ app.layout = rows(
                 ),
                 cols(
                     [
+                        "Model Name",
+                        model_name_selector := dcc.RadioItems(
+                            ["HiddenLayerModel", "HiddenLayerModelVariation"],
+                            "HiddenLayerModel",
+                        ),
+                    ]
+                ),
+                cols(
+                    [
                         "Sparsity",
                         sparsity_selector := dcc.RadioItems(
                             [str(s) for s in sparsities], "0"
@@ -64,7 +75,7 @@ app.layout = rows(
                     [
                         "Task",
                         task_selector := dcc.RadioItems(
-                            ["ID", "SQUARE", "MAX", "MIN"], "ID"
+                            ["ID", "SQUARE", "MAX", "MIN", "ABS"], "ABS"
                         ),
                     ]
                 ),
@@ -73,9 +84,9 @@ app.layout = rows(
                         "Run number",
                         rows(
                             [
-                                run_no_selector := dcc.RadioItems(
+                                run_id_selector := dcc.RadioItems(
                                     [],
-                                    "0",
+                                    "",
                                     style={
                                         "display": "flex",
                                         "flex-direction": "column",
@@ -124,8 +135,8 @@ app.layout = rows(
             "Weights",
             cols(
                 [
-                    fig_w_square := dcc.Graph(figure=go.Figure()),
-                    fig_w := dcc.Graph(figure=go.Figure()),
+                    fig_w1 := dcc.Graph(figure=go.Figure()),
+                    fig_w2 := dcc.Graph(figure=go.Figure()),
                 ]
             ),
         ),
@@ -138,91 +149,163 @@ app.layout = rows(
             "Bias",
             fig_b := dcc.Graph(figure=go.Figure()),
         ),
-        train_result_nums := dcc.Store(id="train-result-nums"),
-        train_result_no := dcc.Store(id="train-result-no"),
+        foldable_section(
+            "Checkpoints",
+            checkpoint_view := dcc.Graph(figure=go.Figure()),
+        ),
+        train_result_ids := dcc.Store(id="train-result-ids"),
+        train_result_id := dcc.Store(id="train-result-no"),
     ]
 )
 
 
 @app.callback(
-    Output(train_result_nums, "data"),
+    Output(train_result_ids, "data"),
+    Input(model_name_selector, "value"),
     Input(act_fn_selector, "value"),
     Input(sparsity_selector, "value"),
     Input(task_selector, "value"),
 )
-def update_run_nums(act_fn, sparsity, task_name):
-    con = sqlite3.connect("new.db")
+def update_run_ids(model_name, act_fn, sparsity, task_name):
+    con = sqlite3.connect("hidden_layer.db")
     cur = con.cursor()
     cur.execute(
         """SELECT run_no FROM training_run
-           WHERE name = 'ToyModel'
+           WHERE name = :model_name
            AND act_fn = :act_fn
            AND sparsity = :sparsity
            AND task = :task_name
         """,
-        dict(act_fn=act_fn, sparsity=sparsity, task_name=task_name),
+        dict(
+            model_name=model_name, act_fn=act_fn, sparsity=sparsity, task_name=task_name
+        ),
     )
 
-    run_nos = cur.fetchall()
-    return json.dumps([i for (i,) in run_nos])
+    run_ids = cur.fetchall()
+    run_ids = [s for (s,) in run_ids]
+    print(run_ids)
+    # run_nos = set(run_nos)
+
+    # deduplicate based on final loss
+    # all_losses = dict()
+    # for i in run_nos:
+    #     train_result = get_result(i)
+    #     all_losses[train_result.losses[-1]] = i
+    # run_nos = all_losses.values()
+
+    return json.dumps(run_ids)
 
 
 @app.callback(
-    Output(run_no_selector, "options"),
-    Output(run_no_selector, "value"),
+    Output(run_id_selector, "options"),
+    Output(run_id_selector, "value"),
     Output(avg_loss, "children"),
-    Input(train_result_nums, "data"),
+    Input(train_result_ids, "data"),
 )
-def update_run_selector(train_result_nums):
-    run_nums = json.loads(train_result_nums)
+def update_run_selector(train_result_ids):
+    run_ids = json.loads(train_result_ids)
 
-    if len(run_nums) == 0:
-        return [], 0, ""
+    if len(run_ids) == 0:
+        return [], "", ""
 
     all_losses = dict()
-    for i in run_nums:
-        train_result = get_result(i)
-        all_losses[i] = train_result.losses[-1]
-    avg_loss = sum(all_losses) / len(all_losses)
+    params = dict()
+    has_checkpoint = dict()
+    for run_id in run_ids:
+        train_result = get_result(run_id, load_checkpoints=False)
+        args = train_result.config.args
+        all_losses[run_id] = train_result.losses[-1]
+        params[run_id] = args["features"], args["neurons"]
+        has_checkpoint[run_id] = os.path.exists(
+            f"train_results/{run_id}/checkpoints.pkl"
+        )
+    avg_loss = sum(all_losses.values()) / len(all_losses)
 
-    # print(f"run_nums: {run_nums}")
-    options = [dict(label=f"{i}: {all_losses[i]:.3f}", value=i) for i in run_nums]
+    options = [
+        (
+            all_losses[run_id],
+            dict(
+                label=f"{run_id[:4]}{'*' if has_checkpoint[run_id] else ''} {params[run_id]}: {all_losses[run_id]:.3f}",
+                value=run_id,
+            ),
+        )
+        for run_id in run_ids
+    ]
+    options.sort(key=lambda x: x[0])
+    options = [x[1] for x in options]
 
     return (
         options,
-        run_nums[0],
+        options[0]["value"],
         f"average loss {avg_loss:.3f}",
     )
 
 
 @app.callback(
-    Output(train_result_no, "data"),
-    Input(train_result_nums, "data"),
-    Input(run_no_selector, "value"),
+    Output(train_result_id, "data"),
+    Input(train_result_ids, "data"),
+    Input(run_id_selector, "value"),
 )
-def get_model(train_result_nums, run_num):
-    run_nums = json.loads(train_result_nums)
-    # print(f"train_result_nums: {train_result_nums}")
-    # print(f"run_nums: {run_nums}")
+def get_run_id(train_result_ids, run_id):
+    run_ids = json.loads(train_result_ids)
 
-    if len(run_nums) == 0:
-        return 1
+    if len(run_ids) == 0:
+        return ""
 
-    # print(f"train_result_no: {run_num}")
-    return int(run_num)
+    return run_id
 
 
 @app.callback(
-    Output(fig_w_square, "figure"),
-    Output(fig_w, "figure"),
+    Output(checkpoint_view, "figure"),
+    Input(train_result_id, "data"),
+)
+def update_checkpoint_view(train_result_id):
+    if not train_result_id:
+        return go.Figure()
+
+    train_result = get_result(train_result_id, load_checkpoints=True)
+    model = train_result.model
+    model.cpu()
+    checkpoints = train_result.checkpoints
+
+    if not checkpoints:
+        return go.Figure()
+
+    ims = np.empty(
+        (len(checkpoints) // 10, model.features, model.features), dtype=np.float32
+    )
+
+    with torch.no_grad():
+        for i, cpkt in enumerate(checkpoints):
+            if i % 10 == 0:
+                model.load_state_dict(cpkt[1])
+                w = model.W.numpy()
+                ims[i // 10] = w.T @ w
+
+    return px.imshow(ims, animation_frame=0, title="w.T @ w")
+
+
+@app.callback(
+    Output(fig_w1, "figure"),
+    Output(fig_w2, "figure"),
     Output(fig_b, "figure"),
     Output(fig_weights_sankey, "figure"),
     Output(ln_plots, "children"),
     Output(fig_loss, "figure"),
-    Input(train_result_no, "data"),
+    Input(train_result_id, "data"),
 )
-def update_plots(train_result_no):
-    train_result = get_result(int(train_result_no))
+def update_plots(train_result_id):
+    if not train_result_id:
+        return (
+            go.Figure(),
+            go.Figure(),
+            go.Figure(),
+            go.Figure(),
+            [],
+            go.Figure(),
+        )
+
+    train_result = get_result(train_result_id, load_checkpoints=False)
 
     with torch.no_grad():
         model = train_result.model.cpu()
@@ -241,8 +324,8 @@ def update_plots(train_result_no):
         )
 
     return (
-        plots["w_square"],
-        plots["w"],
+        plots.get("w", plots.get("w1", None)),
+        plots.get("w2", go.Figure()),
         plots["b"],
         plots["weights_sankey"],
         ln_plots,
@@ -272,7 +355,7 @@ def update_plots(train_result_no):
     Input(input17, "value"),
     Input(input18, "value"),
     Input(input19, "value"),
-    Input(train_result_no, "data"),
+    Input(train_result_id, "data"),
 )
 def update_activations_graph(
     input0,
@@ -295,36 +378,40 @@ def update_activations_graph(
     input17,
     input18,
     input19,
-    train_result_no,
+    train_result_id,
 ):
-    train_result = get_result(int(train_result_no))
-    input = torch.zeros((1, 20))
-    input[0] = torch.tensor(
-        [
-            input0,
-            input1,
-            input2,
-            input3,
-            input4,
-            input5,
-            input6,
-            input7,
-            input8,
-            input9,
-            input10,
-            input11,
-            input12,
-            input13,
-            input14,
-            input15,
-            input16,
-            input17,
-            input18,
-            input19,
-        ]
-    )
+    if not train_result_id:
+        return go.Figure()
+    train_result = get_result(train_result_id, load_checkpoints=False)
+    features = train_result.model.features
+    input = torch.zeros((1, features))
+    inputs = [
+        input0,
+        input1,
+        input2,
+        input3,
+        input4,
+        input5,
+        input6,
+        input7,
+        input8,
+        input9,
+        input10,
+        input11,
+        input12,
+        input13,
+        input14,
+        input15,
+        input16,
+        input17,
+        input18,
+        input19,
+    ]
+    inputs = inputs[:features]
+    input[0][0 : len(inputs)] = torch.tensor(inputs)
     with torch.no_grad():
         model = train_result.model.cpu()
+    model.eval()
     return model.activations_graph(input)
 
 
